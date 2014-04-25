cbb5d7b6d66659fde0c45d013ca9dfdb7eb14ff0
Christopher Jack <3esmile@Chris-Jacks-iMac.local>
2014-04-25 15:39:29 +0800

    featured spots

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $('[data-toggle=offcanvas]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
+
+  $('.bxslider').bxSlider({
+	  mode: 'fade',
+	  captions: true,
+	  adaptiveHeight: true,
+	});
+
+
 });
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,7 @@ class TopController < ApplicationController
   	@spots = Spot.all
   	@spots_recent = Spot.limit(6)
   	@spots_ranking = Spot.limit(6).order('rate desc')
-  	@spots_featured = Spot.where(featured: true).limit(1)
+  	@spots_featured = Spot.where(featured: true).limit(4)
   	@categories = Category.all
   	@areas = Area.all
   end
