95f771fed3885329602a457fdb629e1e217911e8
Christopher Jack <3esmile@Chris-Jacks-iMac.local>
2014-04-25 16:06:35 +0800

    will_paginate added

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -5,6 +5,7 @@ class SpotsController < ApplicationController
     @spots = Spot.all
     @areas = Area.all
     @categories = Category.all
+    @posts = Spot.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
